2023f03b417b27a1dbefb6b0c2ec051f6b5d160f
Mar1kSX <sergey.mareev@gmail.com>
2018-03-23 11:15:02 +0300

    Any changes for fib scripts

--- fib_def.py
+++ fib_def.py
@@ -8,11 +8,6 @@ def isint(value):
     except ValueError:
         return False
 
-def fibV2(n):
-    if n == 1 or n == 2:
-    return fib_list.append(1)
-    return fib_list.append(fib_list[n-1]+fib_list[n-2])
-
 def fibV1(k):
     n = int(k)
     if n == 1: fib_list = [1]
@@ -21,11 +16,10 @@ def fibV1(k):
         for i in range(2, n - 1):
             fib_list.append(fib_list[i - 1] + fib_list[i - 2])
 
-    return fib_list
+    print(fib_list)
 
 if isint(num):
     print('числа Фибоначчи:')
     fibV1(int(num))
-    print(fibV2(5))
 
 else: print('Введеное число не является целым')
